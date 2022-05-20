cd3c8f858450d37812ad1032ceb4b7baea01bd70
Arcticcrunch <45840800+Arcticcrunch@users.noreply.github.com>
2022-05-21 02:57:35 +0400

    Added quicksort

--- CrossInform.FrequencyTextAnalyzerTests/TextUtilsTests.cs
+++ CrossInform.FrequencyTextAnalyzerTests/TextUtilsTests.cs
@@ -33,5 +33,88 @@ namespace CrossInform.FrequencyTextAnalyzer.Tests
             Assert.AreEqual(1, dict[thirdCommon]);
             Assert.ThrowsException<KeyNotFoundException>(() => dict[fifthCharsSequence]);
         }
+
+        [TestMethod()]
+        public void QuickSortTest()
+        {
+            int[] a = new int[] { 3, 4, 0, -5 };
+
+            Quicksort(a, 0, a.Length - 1);
+            Assert.AreEqual(-5, a[0]);
+            Assert.AreEqual(0, a[1]);
+            Assert.AreEqual(3, a[2]);
+            Assert.AreEqual(4, a[3]);
+        }
+
+
+
+
+
+        private void Quicksort(int[] x, int low, int hi)
+        {
+            if (low < hi)
+            {
+                // int referenceElement = Sort(x, low, hi);
+                int referenceElement = 0;
+                
+                int pivot = x[(low + hi) / 2];
+                int i = low;
+                int j = hi;
+                while (true)
+                {
+                    while (x[i] < pivot)
+                    {
+                        i++;
+                    }
+                    while (x[j] > pivot)
+                    {
+                        j--;
+                    }
+                    if (i >= j)
+                    {
+                        referenceElement = j;
+                        break;
+                    }
+                    int temp = x[i];
+                    x[i] = x[j];
+                    x[j] = temp;
+                    i++;
+                    j--;
+                }
+
+                //Sort(x, low, hi);
+                Quicksort(x, low, referenceElement);
+                Quicksort(x, referenceElement + 1, hi);
+            }
+        }
+
+        [Obsolete()]
+        private int Sort(int[] x, int low, int hi)
+        {
+            int pivot = x[(low + hi) / 2];
+            int i = low;
+            int j = hi;
+            while(true)
+            {
+                while(x[i] < pivot)
+                {
+                    i++;
+                }
+                while(x[j] > pivot)
+                {
+                    j--;
+                }
+                if (i >= j)
+                {
+                    return j;
+                }
+                int temp = x[i];
+                x[i] = x[j];
+                x[j] = temp;
+                i++;
+                j--;
+            }
+        }
+        
     }
 }
